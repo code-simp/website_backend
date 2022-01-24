83fd63d0925b9b7f1d0fbf41677e90744383235e
Tarun <84436181+code-simp@users.noreply.github.com>
2022-01-24 20:47:41 +0530

    added cors

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+var cors = require('cors');
 
 const app = express();
-app.use(bodyParser.json)
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.get('/', function (req, res) {
     res.send('works')
 })
 
 app.post('/contact', (req, res) => {
-    console.log(req)
+    console.log(req.body)
     res.send('success')
 })
 
 app.listen(8000, function () {
-    console.log('app started on port 3001')
+    console.log('app started on port 8000')
 })
